76f0739716f3c653223b81a699ea6ea754077c9c
Andrei464 <aasholokhov2003@gmail.com>
2023-11-27 14:07:40 -0600

    awdaw

--- test.sql
+++ test.sql
@@ -23,7 +23,50 @@ create table airlines (
 );
 
 create table airports (
-    airlineID varchar(100) primary key,
-    name text not null,
-    type text not null
+    icaoCode varchar(4) not null primary key,--4 letter code, globally unqiue
+    iataCode varchar(3),--3 letter code
+    airportID text not null,--unique neumeric id
+    name text not null,--name of the airport
+    elevation integer not null,--in feet
+    latitude float not null,--coordinates
+    longitude float not null,--coordinates
+    municipality text not null,--city
+    airportSize text not null,--large mdeium small
+    isoCountry varchar(2) not null,--2 letter code
+    region text not null,--Country-Region
+    continent varchar(2) not null,--NA
+    localCode varchar(3) not null,--3 letter code
+);
+
+create table waypoints(
+    name varchar(5) primary key,
+    latitude float not null,
+    longitude float not null,
+);
+
+create table flightRoutes(
+    routeID integer primary key IDENTITY(1,1),
+    orig varchar(4) not null references airports(icaoCode),
+    dest varchar(4) not null references airports(icaoCode),
+    monthFlown integer,
+    carrier text,
+    airlineID varchar(100) references airlines,
+    passengers integer,
+    distance float,
+    aircraftID text
+);
+
+create table waypointRoutes(
+    id integer primary key references flightRoutes(id),
+    name varchar(5) references waypoints
+);
+
+create table airportsAndAirlines(
+    airlineID varchar(100) references airlines,
+    airport varchar(4) references airports
+);
+
+create table routesAndAirlines(
+    routeID integer references flightRoutes,
+    airlineID varchar(100) references airlines
 );
