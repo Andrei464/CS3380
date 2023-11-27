9a2f8af48b1479fd1cea89414572877542edba75
Andrei464 <aasholokhov2003@gmail.com>
2023-11-27 12:57:27 -0600

    Update test.sql

--- test.sql
+++ test.sql
@@ -1,7 +1,7 @@
 use cs3380
 
 -- Order matters!
-drop table if exists viewed;
+drop table if exists aircraft;
 drop table if exists orderLineItems;
 drop table if exists orders;
 drop table if exists people;
@@ -11,5 +11,19 @@ drop table if exists provinces;
 create table aircraft (
     aircraftID varchar(100) primary key,
     modelNum text not null,
-    typeOfAircraft text not null
+    typeOfAircraft text not null,
+    ownedBy text not null,
+    numberOwned integer not null
+);
+
+create table airlines (
+    airlineID varchar(100) primary key,
+    name text not null,
+    type text not null
+);
+
+create table airports (
+    airlineID varchar(100) primary key,
+    name text not null,
+    type text not null
 );
